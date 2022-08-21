781dc8236739f30809d2d6db03af446c52d31b04
DawidFafara <dawid.fafara95@gmail.com>
2022-08-21 10:56:30 +0200

    Test automation Practice

---
+++ src/test/java/AutomationPracticeTest.java
@@ -0,0 +1,48 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
+import org.junit.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import static org.junit.Assert.assertEquals;
+
+public class AutomationPracticeTest {
+    private WebDriver driver;
+
+    @Before
+    public void setUp()
+    {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("http://automationpractice.com/index.php");
+    }
+
+    @Test
+    public  void dummy()
+    {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @After
+    public void tearDown()
+    {
+        driver.close();
+    }
+}
